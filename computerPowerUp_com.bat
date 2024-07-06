229190849b4834dfca3463e8a6e514b0ea506e59
hongmin0413 <s94062826@gmail.com>
2024-07-06 10:22:07 +0800

    revise path of config.ini

--- computerPowerUp_com.bat
+++ computerPowerUp_com.bat
@@ -18,7 +18,9 @@ if %isOpenEclipse% equ true (
 	set eclipseExe2=D:\APPS\eclipse-jee-2023-06-R\eclipse.exe
 	start "" /min "!eclipseExe1!" -data "D:\workspace\農業部\workspace_MOA_COA" && timeout /t 10 /nobreak>nul
 	start "" /min "!eclipseExe1!" -data "D:\workspace\智慧局\workspace_MOA_TIPO" && timeout /t 10 /nobreak>nul
+	rem start "" /min "!eclipseExe1!" -data "D:\workspace\智慧局_111增修\workspace_MOA_TIPO" && timeout /t 10 /nobreak>nul
 	start "" /min "!eclipseExe1!" -data "D:\workspace\國發會\workspace_MOA_CI" && timeout /t 10 /nobreak>nul
+	rem start "" /min "!eclipseExe1!" -data "D:\workspace\國發會_111增修\workspace_MOA_CI" && timeout /t 10 /nobreak>nul
 	rem start "" /min "!eclipseExe1!" -data "D:\workspace\農險基金\workspace_MOA_農保基金" && timeout /t 10 /nobreak>nul
 	start "" /min "!eclipseExe1!" -data "D:\workspace\經濟部\workspace_MOA_MOEA" && timeout /t 10 /nobreak>nul
 	rem start "" /min "!eclipseExe2!" -data "D:\workspace\經濟部OA\workspace_OASystem_JDK17" && timeout /t 10 /nobreak>nul
@@ -33,13 +35,16 @@ rem 開啟bcompare
 if %isOpenBcompare% equ true (
 	set bcompareExe=D:\Tools\BC\Beyond Compare 4\BCompare.exe
 	rem 先讀取程式設定檔的config.ini 
-	for /f "delims=" %%i in ('type "C:\Project\JavaProject\config.ini"^| find /i "="') do set %%i
-	start "" /min "!bcompareExe!" "MOADomsEE <--> workspace_MOA_COA"
-	"!bcompareExe!" "MOADomsEE <--> workspace_MOA_TIPO"
-	"!bcompareExe!" "MOADomsEE <--> workspace_MOA_CI"
-	"!bcompareExe!" "MOADomsEE <--> workspace_MOA_農保基金"
-	"!bcompareExe!" "MOADomsEE <--> workspace_MOA_MOEA"
-	"!bcompareExe!" "OASystemEE <--> workspace_OASystem_JDK17"
+	rem 113.05.15 調整程式設定檔config.ini的路徑 
+	for /f "delims=" %%i in ('type "C:\Project\JavaProject\更新機關設定檔指令\config.ini"^| find /i "="') do set %%i
+	start "" /min "!bcompareExe!" "MOADomsEE <--> workspace_農業部"
+	"!bcompareExe!" "MOADomsEE <--> workspace_智慧局"
+	rem "!bcompareExe!" "MOADomsEE <--> workspace_智慧局_111增修"
+	"!bcompareExe!" "MOADomsEE <--> workspace_國發會"
+	rem "!bcompareExe!" "MOADomsEE <--> workspace_國發會_111增修"
+	"!bcompareExe!" "MOADomsEE <--> workspace_農險基金"
+	"!bcompareExe!" "MOADomsEE <--> workspace_經濟部"
+	"!bcompareExe!" "OASystemEE <--> workspace_經濟部OA"
 	start "" /min "!bcompareExe!" "!currentOrgName!\MOADoms_settings"
 )
 
